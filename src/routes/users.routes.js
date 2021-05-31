5950a6268402c4dce2a9e5e6dbd342a563a69036
Matheus A. Moreira <matheusmeka01@gmail.com>
2021-05-30 21:28:53 -0300

    more changes on cors

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,7 +3,12 @@ const { usersController } = require("../controllers");
 const { isAuthorized } = require("../middlewares");
 var cors = require('cors');
 
-router.use(cors());
+router.use(cors({
+    "origin": "'https://presente-camp.vercel.app/'",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  }));
 
 router.use(isAuthorized);
 
